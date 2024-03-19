fd86eeb5f140517aa600fbf948533b50c2d9c590
Muhammad-Subhan-Rauf <107776286+Muhammad-Subhan-Rauf@users.noreply.github.com>
2024-03-19 20:37:03 +0500

    Add files via upload

---
+++ wifi_password_getter.py
@@ -0,0 +1,18 @@
+import subprocess
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+for profile in profiles:
+    try:
+
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', profile, 'key=clear']).decode("utf-8").split("\n")
+        password_lines = [line.split(':')[1][1:-1] for line in results if 'Key Content' in line]
+        
+
+        if password_lines:
+            print("{:<30}|  {:<}".format(profile, password_lines[0]))
+        else:
+            print("{:<30}|  {:<}".format(profile, "Password not found"))
+    except:
+        pass
